2b6cbc6d6031c71be5b05743d6579e365d9a40aa
v2d <12630725+vlad-isan@users.noreply.github.com>
2022-03-04 13:54:09 +0000

    test

--- .config/nvim/lua/plugins/gitsigns/config.lua
+++ .config/nvim/lua/plugins/gitsigns/config.lua
@@ -11,14 +11,42 @@ gitsigns.setup({
     changedelete = { text = sign.line.SHADE.light },
   },
   on_attach = function(bufnr)
-    -- NOTE: Wokraround until gitsigns command accept args
+    ---- NOTE: Wokraround until gitsigns command accept args
     require("lib.command").add("Blame", function()
       gitsigns.blame_line({ full = true })
     end, { buf = bufnr })
     require("plugins.gitsigns.keymap").attach(bufnr)
   end,
-  preview_config = { border = "rounded" },
-  current_line_blame = true,
-  current_line_blame_opts = { delay = 250 },
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true
+  },
+  attach_to_untracked = true,
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
   current_line_blame_formatter = formatter.current_line_blame,
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'rounded',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  yadm = {
+    enable = false
+  },
 })
