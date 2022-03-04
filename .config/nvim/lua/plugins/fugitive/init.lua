d62fb584cd4991cea338e4697ebab9c6efa0c5dc
v2d <12630725+vlad-isan@users.noreply.github.com>
2022-03-04 16:23:45 +0000

    fugitive

--- .config/nvim/lua/plugins/fugitive/init.lua
+++ .config/nvim/lua/plugins/fugitive/init.lua
@@ -1,8 +1,13 @@
 return {
     "tpope/vim-fugitive",
-    config = function()
-        require("plugins.fugitive.keymap")
-    end,
-    module = "fugitive",
-    opt = false
+    --config = function()
+    --    require("plugins.fugitive.keymap")
+    --end,
+    --module = "fugitive",
+    opt = false,
+    cmd = {
+        "G", "GcLog", "Gclog", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit",
+        "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
+        "Gdelete", "Gremove", "Gbrowse",
+    }
 }
