ef339cef893df23dc950e379bb2ac52bd39afaa4
v2d <12630725+vlad-isan@users.noreply.github.com>
2022-10-30 12:33:24 +0000

    Updates launch scripts

--- kitty-launch-eurostar-session.sh
+++ kitty-launch-eurostar-session.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup kitty --session ./.config/kitty/conf.d/eurostar-session.conf < /dev/null > /dev/null 2>&1 &
+nohup kitty --session ./conf.d/eurostar-session.conf < /dev/null > /dev/null 2>&1 &
 disown
--- kitty-launch-storengy-session.sh
+++ kitty-launch-storengy-session.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup kitty --session ./.config/kitty/conf.d/storengy-session.conf < /dev/null > /dev/null 2>&1 &
+nohup kitty --session ./conf.d/storengy-session.conf < /dev/null > /dev/null 2>&1 &
 disown
